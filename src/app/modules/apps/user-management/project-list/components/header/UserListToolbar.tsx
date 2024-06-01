11916315450562214ecd09dfca9c874566928e75
NurettinKaynar <nurettinkaynar568@gmail.com>
2024-06-01 12:04:57 +0300

    proje Listesi ekleme alanı düzenlendi

--- src/app/modules/apps/user-management/project-list/components/header/UserListToolbar.tsx
+++ src/app/modules/apps/user-management/project-list/components/header/UserListToolbar.tsx
@@ -1,32 +1,37 @@
-import {KTIcon} from '../../../../../../../_metronic/helpers'
-import {useListView} from '../../core/ListViewProvider'
-import {UsersListFilter} from './UsersListFilter'
+import { KTIcon } from "../../../../../../../_metronic/helpers";
+import { useListView } from "../../core/ListViewProvider";
+import { UsersListFilter } from "./UsersListFilter";
 
 const UsersListToolbar = () => {
-  const {setItemIdForUpdate} = useListView()
+  const { setItemIdForUpdate } = useListView();
   const openAddUserModal = () => {
-    setItemIdForUpdate(null)
-  }
+    setItemIdForUpdate(null);
+  };
 
   return (
-    <div className='d-flex justify-content-end' data-kt-user-table-toolbar='base'>
-      <UsersListFilter />
+    <div
+      className="d-flex justify-content-end"
+      data-kt-user-table-toolbar="base">
+      {/* <UsersListFilter /> */}
 
       {/* begin::Export */}
-      <button type='button' className='btn btn-light-primary me-3'>
-        <KTIcon iconName='exit-up' className='fs-2' />
-        Export
+      <button type="button" className="btn btn-light-primary me-3">
+        <KTIcon iconName="exit-up" className="fs-2" />
+        Çıktı Al
       </button>
       {/* end::Export */}
 
       {/* begin::Add user */}
-      <button type='button' className='btn btn-primary' onClick={openAddUserModal}>
-        <KTIcon iconName='plus' className='fs-2' />
-        Add User
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={openAddUserModal}>
+        <KTIcon iconName="plus" className="fs-2" />
+        Proje Ekle
       </button>
       {/* end::Add user */}
     </div>
-  )
-}
+  );
+};
 
-export {UsersListToolbar}
+export { UsersListToolbar };

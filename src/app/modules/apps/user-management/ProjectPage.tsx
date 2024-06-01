b011c212df81528ca82cb9ca589965bef51ff58f
NurettinKaynar <nurettinkaynar568@gmail.com>
2024-06-01 12:03:06 +0300

    userPage ProjectPage olarak değiştirildi

---
+++ src/app/modules/apps/user-management/ProjectPage.tsx
@@ -0,0 +1,41 @@
+import { Route, Routes, Outlet, Navigate } from "react-router-dom";
+import { PageLink, PageTitle } from "../../../../_metronic/layout/core";
+import { ProjectListWrapper } from "./project-list/UsersList";
+
+const usersBreadcrumbs: Array<PageLink> = [
+  {
+    title: "Proje Listesi",
+    path: "/project-operation/projects",
+    isSeparator: false,
+    isActive: false,
+  },
+  {
+    title: "",
+    path: "",
+    isSeparator: true,
+    isActive: false,
+  },
+];
+
+const UsersPage = () => {
+  return (
+    <Routes>
+      <Route element={<Outlet />}>
+        <Route
+          path="projects"
+          element={
+            <>
+              <PageTitle breadcrumbs={usersBreadcrumbs}>
+                Proje Listesi
+              </PageTitle>
+              <ProjectListWrapper />
+            </>
+          }
+        />
+      </Route>
+      <Route index element={<Navigate to="/project-operation/projects" />} />
+    </Routes>
+  );
+};
+
+export default UsersPage;
--- src/app/modules/profile/ProfileHeader.tsx
+++ src/app/modules/profile/ProfileHeader.tsx
@@ -1,18 +1,17 @@
-
-import { FC } from 'react'
-import {KTIcon, toAbsoluteUrl} from '../../../_metronic/helpers'
-import {Link, useLocation} from 'react-router-dom'
-import {Dropdown1} from '../../../_metronic/partials'
-import { ToolbarWrapper } from '../../../_metronic/layout/components/toolbar'
-import { Content } from '../../../_metronic/layout/components/content'
+import { FC } from "react";
+import { KTIcon, toAbsoluteUrl } from "../../../_metronic/helpers";
+import { Link, useLocation } from "react-router-dom";
+import { Dropdown1 } from "../../../_metronic/partials";
+import { ToolbarWrapper } from "../../../_metronic/layout/components/toolbar";
+import { Content } from "../../../_metronic/layout/components/content";
 
 const ProfileHeader: FC = () => {
-  const location = useLocation()
+  const location = useLocation();
 
   return (
     <>
       <ToolbarWrapper />
-      <Content>
+      {/* <Content>
         <div className='card mb-5 mb-xl-10'>
           <div className='card-body pt-9 pb-0'>
             <div className='d-flex flex-wrap flex-sm-nowrap mb-3'>
@@ -202,9 +201,9 @@ const ProfileHeader: FC = () => {
             </div>
           </div>
         </div>
-      </Content>
+      </Content> */}
     </>
-  )
-}
+  );
+};
 
-export {ProfileHeader}
+export { ProfileHeader };

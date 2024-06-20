cb25e0877dab0ec99a5afff13c2c07e9bd8cf372
Nurettin KAYNAR <nurettinkaynar568@gmail.com>
2024-06-21 02:27:40 +0300

    error düzeltildi

--- src/app/task-management/core/_requests.ts
+++ src/app/task-management/core/_requests.ts
@@ -5,26 +5,26 @@ import { ApiUrls } from "../../utilities/ApiService";
 const API_URL = ApiUrls.BASE_URL;
 
 
-const getTaskList=(query:Object):Promise<Axios>=>{
+const getTaskList=(query:Object):Promise<any>=>{
     return axios.get(`${API_URL}${ApiUrls.GET_TASK_LIST}`,{
         params:query
     })
 }
 
-const getTaskById=(taskId:number):Promise<Axios>=>{
+const getTaskById=(taskId:number):Promise<any>=>{
     return axios.get(`${API_URL}${ApiUrls.GET_TASK_BY_ID}`,{
         params:{id:taskId}
     })
 }
-const createTask=(task:Object):Promise<Axios>=>{
+const createTask=(task:Object):Promise<any>=>{
     return axios.post(`${API_URL}${ApiUrls.CREATE_TASK}`,{task})
 } 
 
-const updateTask=(task:Object):Promise<Axios>=>{
+const updateTask=(task:Object):Promise<any>=>{
     return axios.put(`${API_URL}${ApiUrls.UPDATE_TASK}`,{task})
 }
 
-const deleteTaskById=(taskId:number):Promise<Axios>=>{
+const deleteTaskById=(taskId:number):Promise<any>=>{
     return axios.delete(`${API_URL}${ApiUrls.DELETE_TASK}`,{
         params:{id: taskId}
     })
